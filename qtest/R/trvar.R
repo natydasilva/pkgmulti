f83e771b3d2be53eb2af58ce605599c721450c7f
natydasilva <ndasilva@iastate.edu>
2014-12-05 18:58:48 -0600

    ss

--- qtest/R/trvar.R
+++ qtest/R/trvar.R
@@ -8,9 +8,17 @@
 #' x <- rmvnorm(50,mean=c(1,2,3),3*diag(3))
 #' y <- rmvnorm(40,mean=c(1,2,3)+5,diag(3))
 #' q_stat(x,y)
-tr_var <- function(data,j,k){
-n <- dim(data)[[1]]
-m <- (n/(n-2))*(apply(data,2,mean)-(data[j,]+data[k,]))
+tr_var <- function(data){
+  n <- dim(data)[[1]]
 
-trvar <- sum(diag((data[-k,]-m)%*%t(data)%*%t((data[-k,]-m)%*%t(data))))
+  tr_jk <- function(j,k) {
+    m <- (n/(n-2))*(apply(data,2,mean)-(data[j,]+data[k,]))
+    sum(t(tcrossprod( t(data[-k,]) - m , t(data[-k,])) ) * tcrossprod( t(data[-j,]) - m , t(data[-j,]) )  )
+  }
+
+  mm <- matrix(1:(n*n), ncol=n)
+  aux <- mm[lower.tri(mm)]
+  ind <- expand.grid(j=1:n, k=1:n)[aux,]
+
+  trs <- mdply(ind, tr_jk)
 }
